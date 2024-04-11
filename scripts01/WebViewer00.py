32ff944eee4c51a6c3efabc279a58abaaec188a3
Hugo S <hugosantos97@outlook.pt>
2024-04-11 16:29:17 +0100

    page receives and shows params

--- scripts01/WebViewer00.py
+++ scripts01/WebViewer00.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', val1="666")
 
 
 def main():
-    app.run(port=5001, debug=True, host='0.0.0.0')
+    app.run(port=5005, debug=True, host='0.0.0.0')
 
 if __name__ == '__main__':
     main()
