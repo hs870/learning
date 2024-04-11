ef9c95e3591fc829f05dd5f95e0758bed541c0aa
Hugo S <hugosantos97@outlook.pt>
2024-04-11 17:44:28 +0100

    post and get buttons fetch pages

--- scripts01/WebViewer00.py
+++ scripts01/WebViewer00.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html', val1=676, val2="333")
+    return render_template('index.html', val1=666, val2="333")
+
+@app.route('/Go', methods=["GET", "POST"])
+def test_page():
+    if request.method == 'GET':
+        print("Get was submitted")
+        return render_template('get.html')
+    elif request.method == 'POST':
+        print("Post was submitted")
+        return render_template('post.html')
 
 
 def main():
