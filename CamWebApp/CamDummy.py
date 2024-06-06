82d5dd9b741913ed70e25f762b6d477ce683b276
Hugo S <hugosantos97@outlook.pt>
2024-06-06 18:38:52 +0100

    fetch para fazer get a dar, logos ja colocados

--- CamWebApp/CamDummy.py
+++ CamWebApp/CamDummy.py
@@ -2,8 +2,11 @@ import requests
 import json
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello():
@@ -13,6 +16,20 @@ def hello():
 def reply():
     return jsonify(status = "pass")
 
+@app.route('/lens/focus', methods=['GET', 'POST'])
+def lens_Focus():
+    if request.method == 'GET':
+        return jsonify(status = "pass", value = "99")
+
+@app.route('/lens/aperture', methods=['GET', 'POST'])
+def lens_Aperture():
+    if request.method == 'GET':
+        return jsonify(status = "pass", value = "55")
+
+@app.route('/lens/zoom', methods=['GET', 'POST'])
+def lens_Zoom():
+    if request.method == 'GET':
+        return jsonify(status = "pass", value = "42")
 
 def main():
     app.run(port=5000, debug=True, host='0.0.0.0')
--- CamWebApp/static/docs/js/functions.js
+++ CamWebApp/static/docs/js/functions.js
@@ -4,15 +4,28 @@ function post_request(data)
     pedido.open("POST", "/", true);
     pedido.setRequestHeader("Content-Type", "application/json");
     pedido.send(JSON.stringify(data));
+
 }
 
 
 let swirVal = false;
 
-function initialValues(){
+async function initialValues(){
     let ap = 33;
     let zo = 11;
-    let fo = 88;
+    let fo = 44;
+
+    let answer = "whatsup";
+    const response = await fetch("http://127.0.0.1:5000/lens/focus")
+    const values = await response.json();
+    console.log(values);
+    console.log(values["status"]);
+    console.log(values["value"]);
+    if( values["status"] == "pass"){
+        zo = values["value"];
+        console.log("passou");
+    }
+
     document.getElementById("aperture").setAttribute("value", ap);
     document.getElementById("zoom").setAttribute("value", zo);
     document.getElementById("focus").setAttribute("value", fo);
