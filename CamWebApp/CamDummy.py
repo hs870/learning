9ec4df39ecbdab88a79fcfd1313968530d2ebd91
Hugo S <hugosantos97@outlook.pt>
2024-06-13 10:47:03 +0100

    dummy updates/saves new values in between refreshes

--- CamWebApp/CamDummy.py
+++ CamWebApp/CamDummy.py
@@ -22,25 +22,43 @@ def reply():
 
 @app.route('/lens/focus', methods=['GET', 'POST'])
 def lens_Focus():
+    global focus
     if request.method == 'GET':
         return jsonify(status = "pass", value = focus)
     elif request.method == 'POST':
         print("Dummy got a post")
         info = request.json
         print (info)
-        print("dummy will attempt to return")
+        print("valor de focus era")
+        print(focus)
+        print("novo devera ser")
+        print(info["val"])
+        dummy = info["val"]
+        print("valor de focus atualizado para")
+        print(dummy)
+        focus = dummy
+        print(focus)
         return jsonify(status = "pass")
 
 
 @app.route('/lens/piris', methods=['GET', 'POST'])
 def lens_Piris():
+    global piris
     if request.method == 'GET':
         return jsonify(status = "pass", value = piris)
+    elif request.method == 'POST':
+        info = request.json
+        piris = info["val"]
+        return jsonify(status = "pass")
 
 @app.route('/lens/zoom', methods=['GET', 'POST'])
 def lens_Zoom():
+    global zoom
     if request.method == 'GET':
         return jsonify(status = "pass", value = zoom)
+    elif request.method == 'POST':
+        zoom = request.json["val"]
+        return jsonify(status = "pass")
 
 def main():
     app.run(port=5000, debug=True, host='0.0.0.0')
